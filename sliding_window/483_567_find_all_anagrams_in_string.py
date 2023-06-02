80dd978f9a5f5ab98702d030789135350401b453
Akash Gupta <akash.gupta1@olx.com>
2023-06-02 12:41:57 +0530

    questions added

---
+++ sliding_window/483_567_find_all_anagrams_in_string.py
@@ -0,0 +1,41 @@
+"""
+https://leetcode.com/problems/find-all-anagrams-in-a-string/description/
+
+maintain two hashes
+one for p and other for sliding s
+if hashes match they are anagrams
+
+"""
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+
+        h1 = [0 ] *26
+        k = len(p)
+
+        for i in p:
+            idx = ord(i) - 97
+            h1[idx] += 1
+
+        ans = []
+
+        i = 0
+        j = i
+        h2 = [0 ] *26
+        while i <= len(s ) -k and j < len(s):
+            # print(j)
+            idx = ord(s[j]) - 97
+            h2[idx] += 1
+            # print("len:", j- i + 1)
+            if ( j- i + 1) == k:
+                # print("here")
+                if h1 == h2:
+                    ans.append(i)
+                idx = ord(s[i]) - 97
+                h2[idx] -= 1
+                i += 1
+                j += 1
+            else:
+                j += 1
+
+        return ans
+
